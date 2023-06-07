552c4cb8bb1048e2eec9d3b18a877a877c2e62a5
turigon23 <arturoglez_23@hotmail.com>
2023-06-07 09:05:53 -0600

    correct client

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const app = express()
 
 dotenv.config({ path: 'src/.env' })
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: process.env.CLIENT, credentials: true }))
 app.use(morgan('tiny'))
 app.disable('x-powered-by')
 
