7b8489c7e1d481aeab1e6a318527176c3fd4ac48
alonso <glezalonso23@gmail.com>
2023-07-28 13:06:39 -0600

    league mode

--- src/controllers/League.controllers.js
+++ src/controllers/League.controllers.js
@@ -1,5 +1,6 @@
 const LeagueModel = require('../models/League.model')
 const ObjectId = require('mongoose').Types.ObjectId
+const { upload } = require('../libs/cloudinary')
 
 const getLeagues = (req, res) => {
     LeagueModel.find()
@@ -30,34 +31,87 @@ const getLeague = (req, res) => {
     }
 }
 
-const createLeague = (req, res) => {
-    const { league, description, sport, poster } = req.body
-    const newLeague = new LeagueModel({
-        league,
-        description,
-        poster,
-        sport,
-    })
-    newLeague
-        .save()
-        .then((data) => res.status(201).json(data))
-        .catch((error) =>
-            res
-                .status(501)
-                .json({ message: 'Hubo un error al crear la liga ', error })
-        )
+const createLeague = async (req, res) => {
+    const { league, description, sport } = req.body
+    const { poster } = req.files
+    if (poster) {
+        try {
+            const { secure_url } = await upload(poster.tempFilePath)
+            const newLeague = new LeagueModel({
+                league,
+                description,
+                poster: secure_url,
+                sport,
+            })
+            newLeague
+                .save()
+                .then((data) => res.status(201).json(data))
+                .catch((error) =>
+                    res.status(501).json({
+                        message: 'Hubo un error al crear la liga ',
+                        error,
+                    })
+                )
+        } catch (error) {
+            res.status(501).json({
+                message: 'Hubo un error al crear la liga ',
+                error,
+            })
+        }
+    } else {
+        const newLeague = new LeagueModel({
+            league,
+            description,
+            sport,
+        })
+        newLeague
+            .save()
+            .then((data) => res.status(201).json(data))
+            .catch((error) =>
+                res
+                    .status(501)
+                    .json({ message: 'Hubo un error al crear la liga ', error })
+            )
+    }
 }
 
 const updateLeague = async (req, res) => {
     const { id } = req.params
-    const { league, description, sport, poster } = req.body
-    if (ObjectId.isValid(id)) {
+    const { league, description, sport } = req.body
+    const { poster } = req.files
+    if (poster) {
+        try {
+            const { secure_url } = await upload(poster.tempFilePath)
+            await LeagueModel.findOneAndUpdate(
+                { _id: id },
+                {
+                    league,
+                    description,
+                    poster: secure_url,
+                    sport,
+                },
+                { new: true }
+            )
+                .then((data) => res.status(201).json(data))
+                .catch((error) =>
+                    res.status(501).json({
+                        message: 'No se podido actualizar la liga ',
+                        error,
+                    })
+                )
+        } catch (error) {
+            res.status(501).json({
+                message: 'No se podido actualizar la liga ',
+                error,
+            })
+        }
+    } else {
         await LeagueModel.findOneAndUpdate(
             { _id: id },
             {
                 league,
                 description,
-                poster,
+
                 sport,
             },
             { new: true }
@@ -69,10 +123,6 @@ const updateLeague = async (req, res) => {
                     error,
                 })
             )
-    } else {
-        res.status(501).json({
-            messsage: 'Ha ocurrido un error en la peticion',
-        })
     }
 }
 
